183f1f7f72d70a46e891f0f5c06f26c04533e023
clreolve <46010629+clreolve@users.noreply.github.com>
2022-08-09 17:42:05 -0500

    backend start

---
+++ Proyecto3/backend/server_api_rest/routes/api.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+//var {} = require("");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "API" });
+});
+
+let usuario = {
+  nombre: "",
+  apellido: "",
+};
+
+router.get("/usuario", function (req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  res.json(usuario);
+});
+
+module.exports = router;
