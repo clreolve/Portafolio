a14de8541ea019a903fadeca2005c6d4fc52aec3
Reynaldo Olvera <46010629+clreolve@users.noreply.github.com>
2022-08-12 21:08:27 +0000

    routes ready

--- Proyecto3/frontend/app/src/app/app.module.ts
+++ Proyecto3/frontend/app/src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+//Angular material
+
+//my components
+import { MainLayoutComponent } from './templates/main-layout/main-layout.component';
+import { NavbarComponent } from './templates/partials/navbar/navbar.component';
+import { ErrorComponent } from './error/error.component';
+import { TaskComponent } from './task/task.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GroupsComponent } from './groups/groups.component';
+import { ChatsComponent } from './chats/chats.component';
+import { SettingsComponent } from './settings/settings.component';
+
+//-------------------------
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    NavbarComponent,
+
+    //routes
+    ErrorComponent,
+    TaskComponent,
+    GroupsComponent,
+    ChatsComponent,
+    SettingsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
