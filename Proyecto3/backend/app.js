b93520354cf187d349f091973e16320315621ec4
clreolve <clreolve@espol.edu.ec>
2022-09-03 19:03:23 -0500

    rehaciendo el servidor desde cero

--- Proyecto3/backend/app.js
+++ Proyecto3/backend/app.js
@@ -3,17 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
 
-
-//rutas
-const indexRouter = require('./routes/index');
-const documentationRouter = require('./routes/documentation');
-const apiRouter = require('./routes/api');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,10 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Rutas
-app.use('/', documentationRouter);
-app.use('/documentation', documentationRouter);
-app.use('/api', apiRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Proyecto3/backend/routes/users.js
+++ Proyecto3/backend/routes/users.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  //res.send('respond with a resource');
-  return res.render('documentation', {title: "Documentación"})
+  res.send('respond with a resource');
 });
 
 module.exports = router;
